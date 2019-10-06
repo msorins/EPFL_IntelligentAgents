906c6cb2c72714fc9fd25165dba1349fbe3b08ba
Cosmin Rusu <cr.rusucosmin@gmail.com>
2019-10-06 20:12:56 +0200

    Added tasks to README and change the name of the agent

--- Exercise2/workspace/reactive/src/main/Main.java
+++ Exercise2/workspace/reactive/src/main/Main.java
@@ -5,7 +5,7 @@ import logist.LogistPlatform;
 public class Main {
   public static void main(String[] args) {
     String randomAgent[] = {"config/reactive.xml", "reactive-random"};
-    String reactiveAgent[] = {"config/reactive.xml", "reactive-agent"};
+    String reactiveAgent[] = {"config/reactive.xml", "reactive-rla"};
     LogistPlatform.main(reactiveAgent);
   }
 }
