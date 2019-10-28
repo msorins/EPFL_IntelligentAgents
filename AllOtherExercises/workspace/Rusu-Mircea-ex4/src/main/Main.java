121b420bcf10719497f246c5b97976e37d15346a
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2019-10-28 10:56:30 +0100

    Bootstrap Exercise 4

---
+++ AllOtherExercises/workspace/Rusu-Mircea-ex4/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+import logist.LogistPlatform;
+
+public class Main {
+    public static void main(String[] args) {
+        String randomAgent[] = {"config/centralized.xml", "centralized-random"};
+        String centralizedAgent[] = {"config/centralized.xml", "centralized-main"};
+        LogistPlatform.main(randomAgent);
+    }
+}
